f921d3477d9eb27a45bba19736c55168e9f0c622
nicolasdavidgilbert <nico@farobag.com>
2024-12-11 09:54:17 +0000

    Archivo conseguir_imagen_base64 consigue una imágen según el id del prpoducto para no tener que descargarla localemte, el código necesario para implemetnarlo en flet está al final del documento

---
+++ project/views/conseguir_imagen_base64.py
@@ -0,0 +1,29 @@
+from mapeo_colecciones import *
+import base64
+
+# Este archivo recupera una imágen de un producto según su id sin necesidad de gaurdala localmente
+
+
+def obtener_imagen_base64(id) -> base64 or None:
+    producto = Product.objects(_id=id).first()
+    if producto and producto.image:
+        # Leer la imagen como bytes
+        image_bytes = producto.image.read()
+        # Convertir a base64
+        print("Imágen recuperada en base 64")
+        return base64.b64encode(image_bytes).decode('utf-8')
+    else:
+        print(f"No se encontró imagen para el producto con ID {id}.")
+        return None
+
+
+######################################################################################################
+######################################################################################################
+# Para que la imágen se vea en el fet deben de existir las siguientes líneas
+#   image_base64 = obtener_imagen_base64(product_id)
+#   if image_base64:
+#       image_data_url = f"data:image/jpeg;base64,{image_base64}"
+#       img = ft.Image(src=image_data_url, width=300, height=300)
+#       page.add(img)
+######################################################################################################
+######################################################################################################
