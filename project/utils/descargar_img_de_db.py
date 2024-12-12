53c930320d66cc5640c41d3c04c5885e11d1f0bd
nicolasdavidgilbert <nico@farobag.com>
2024-12-12 19:51:37 +0000

    Descargar imagenes en local de la maera correcta

--- project/utils/descargar_img_de_db.py
+++ project/utils/descargar_img_de_db.py
@@ -2,7 +2,6 @@ from project.models.mapeo_colecciones import *
 from project.database.conection import conection
 import os
 
-
 conection = conection()
 # Asegúrate de que la carpeta donde guardarás las imágenes existe
 os.makedirs("../imagens", exist_ok=True)
@@ -13,7 +12,7 @@ def descargar_imagen():
     for producto in Product.objects():
         if producto and producto.image:
             # Lee la imagen desde la base de datos
-            with open(f"imagenes/producto_{producto._id}.jpg", "wb") as archivo_imagen:
+            with open(f"../imagens/producto_{producto._id}.jpg", "wb") as archivo_imagen:
                 archivo_imagen.write(producto.image.read())
             print(f"Imagen descargada correctamente en 'imagenes/producto_{producto._id}.jpg'")
         else:
