7b0f5e28d4ef70087a15040e45e8d7ea48b311a2
nicolasdavidgilbert <nico@farobag.com>
2025-01-09 13:06:23 +0000

    Añadido guardado de datos de usuario en json

---
+++ project/user/guardar_datos.py
@@ -0,0 +1,15 @@
+from project.models.mapeo_colecciones import User
+import json
+from project.database.conection import conection
+
+from mongoengine import get_db
+
+def crear_json(user:dict):
+    user["_id"] = str(user["_id"])
+    user["registration_date"] = str(user["registration_date"])
+    print(user)
+    # Guardar en un archivo JSON
+    with open('../user/user_data.json', 'w') as json_file:
+        json.dump(user, json_file, indent=4)
+
+    print("Archivo user_data.json creado exitosamente.")
